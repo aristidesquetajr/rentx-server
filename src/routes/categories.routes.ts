9ae30727c4d18bae690b622ed68bbe7cb3b4130b
Aristides Pascoal Queta <aristidesquetajr@gmail.com>
2023-07-16 21:23:37 +0100

    create repository of Category

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,23 +1,14 @@
 import { Router } from 'express'
 
-import { Category } from '../model/Category'
+import { CategoriesRepository } from '../repositories/CategoriesRepository'
 
 const categoriesRoutes = Router();
-
-const categories: Category[] = []
+const categoriesRepository = new CategoriesRepository()
 
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body
 
-  const category = new Category()
-
-  Object.assign(category, {
-    name,
-    description,
-    created_at: new Date()
-  })
-
-  categories.push(category)
+  categoriesRepository.create({ name, description })
 
   return response.status(201).send()
 })
