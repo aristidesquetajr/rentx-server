4b46ea990068a3f2f65091b315e44bcbb6c4d415
Aristides Pascoal Queta <aristidesquetajr@gmail.com>
2023-07-17 14:23:12 +0100

    apply liskov substitution principle

--- src/repositories/CategoriesRepository.ts
+++ src/repositories/CategoriesRepository.ts
@@ -1,11 +1,6 @@
 import { Category } from '../model/Category'
-
-interface ICreateCategoryDTO {
-  name: string
-  description: string
-}
-
-class CategoriesRepository {
+import { ICategoriesRepository, ICreateCategoryDTO } from './ICategoriesRepository'
+class CategoriesRepository implements ICategoriesRepository {
   private categories: Category[]
 
   constructor() {
