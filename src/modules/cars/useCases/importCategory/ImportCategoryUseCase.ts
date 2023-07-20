e53d05a1080c7156afc6a72922ea87e042e8895b
Aristides Pascoal Queta <aristidesquetajr@gmail.com>
2023-07-20 08:49:32 +0100

    added: stream on category import

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,8 +1,18 @@
-
+import { parse } from 'csv-parse'
+import fs from 'fs'
 
 class ImportCategoryUseCase {
-  execute(file: any) {
-    console.log(file)
+  execute(file: Express.Multer.File) {
+    const stream = fs.createReadStream(file.path)
+
+    const fileParse = parse()
+
+    stream.pipe(fileParse)
+
+    fileParse.on("data", async (file) => {
+      console.log(file)
+    })
+
   }
 }
 
