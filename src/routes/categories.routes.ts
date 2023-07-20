e2de472dee9a71f70ee564577147c95d1aef724e
Aristides Pascoal Queta <aristidesquetajr@gmail.com>
2023-07-20 08:17:45 +0100

    added: use case that import category

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -4,6 +4,7 @@ import multer from 'multer'
 
 import { createCategoryController } from '../modules/cars/useCases/createCategory'
 import { listCategoriesController } from '../modules/cars/useCases/listCategories'
+import { importCategoryController } from '../modules/cars/useCases/importCategory'
 
 const categoriesRoutes = Router();
 
@@ -20,11 +21,7 @@ categoriesRoutes.get("/", (request, response) => {
 })
 
 categoriesRoutes.post("/import", upload.single("file"), (request, response) => {
-  const { file } = request
-
-  console.log(file)
-
-  return response.send()
+  return importCategoryController.handle(request, response)
 })
 
 export { categoriesRoutes }
