f44c6d1a5c15d1bf60e4a44e48e5e7cc20f4b38e
Aristides Pascoal Queta <aristidesquetajr@gmail.com>
2023-08-20 19:29:44 +0100

    added: util for delete files

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -1,5 +1,6 @@
 import { inject, injectable } from 'tsyringe'
 
+import { deleteFile } from '../../../../utils/file'
 import { IUsersRepository } from '../../repositories/IUsersRepository'
 
 interface IRequest {
@@ -15,6 +16,12 @@ class UpdateUserAvatarUseCase {
   ) {}
 
   async execute({ id, avatar }: IRequest): Promise<void> {
+    const user = await this.usersRepository.findById(id)
+
+    if (user.avatar) {
+      await deleteFile(`./tmp/avatar/${user.avatar}`)
+    }
+
     await this.usersRepository.updateAvatar({ id, avatar })
   }
 }
