8c2d2d71a97af4aded215b92198ea8e707fb291c
Aristides Pascoal Queta <aristidesquetajr@gmail.com>
2023-08-22 17:36:45 +0100

    added: tests in the creation of categories

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -1,15 +1,45 @@
-describe('Criar categoria', () => {
-  it('Espero que 2 + 2 seja 4', () => {
-    const soma = 2 + 2
-    const resultado = 4
+import 'reflect-metadata'
 
-    expect(soma).toBe(resultado)
+import { AppError } from '../../../../errors/AppError'
+import { CategoriesRepositoryInMemory } from '../../repositories/in-memory/CategoriesRepositoryInMemory'
+import { CreateCategoryUseCase } from './CreateCategoryUseCase'
+
+let createCategoryUseCase: CreateCategoryUseCase
+let categoriesRepositoryInMemory: CategoriesRepositoryInMemory
+
+describe('Create Category', () => {
+  beforeEach(() => {
+    categoriesRepositoryInMemory = new CategoriesRepositoryInMemory()
+    createCategoryUseCase = new CreateCategoryUseCase(
+      categoriesRepositoryInMemory,
+    )
   })
 
-  it('Espero que 2 + 2 não seja 5', () => {
-    const soma = 2 + 2
-    const resultado = 5
+  it('should be able to create a new category', async () => {
+    const category = {
+      name: 'Category Test',
+      description: 'Category description Test',
+    }
 
-    expect(soma).not.toBe(resultado)
+    await createCategoryUseCase.execute(category)
+
+    const categoryCreated = await categoriesRepositoryInMemory.findByName(
+      category.name,
+    )
+
+    expect(categoryCreated).toHaveProperty('id')
+  })
+
+  it('should not be able to create a new category with name exists', async () => {
+    expect(async () => {
+      const category = {
+        name: 'Category Test',
+        description: 'Category description Test',
+      }
+
+      await createCategoryUseCase.execute(category)
+
+      await createCategoryUseCase.execute(category)
+    }).rejects.toBeInstanceOf(AppError)
   })
 })
